3b62016b43f73d47e125b4a075153d316ee8514c
Koderpad <shen882002@gmai.com>
2024-04-08 16:57:21 +0700

    update component

---
+++ src/components/common/Layouts/Stack/index.tsx
@@ -0,0 +1,10 @@
+import { Box, BoxProps } from "../Box";
+import { cn } from "@/utils/cn";
+
+type StackProps = BoxProps;
+
+export const Stack = ({ className, ...props }: StackProps) => {
+  return (
+    <Box className={cn("flex flex-col items-start", className)} {...props} />
+  );
+};
