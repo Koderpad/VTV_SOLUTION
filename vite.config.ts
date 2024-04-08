9148b1a4100abd9c1a384e54f0cf8f1d2a65dad9
Koderpad <shen882002@gmai.com>
2024-04-08 17:53:27 +0700

    update component

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+// import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [react()],
-})
+});
