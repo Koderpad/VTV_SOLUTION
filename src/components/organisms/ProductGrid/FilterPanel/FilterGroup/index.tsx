f05a5dc329a323041b74320ac3aa55f7e2de9acf
Koderpad <shen882002@gmai.com>
2024-04-18 09:06:02 +0700

    update product filter, group in Product grid component

---
+++ src/components/organisms/ProductGrid/FilterPanel/FilterGroup/index.tsx
@@ -0,0 +1,49 @@
+import { FC, useState } from "react";
+
+interface FilterGroupProps {
+  name: string;
+  children: React.ReactNode;
+}
+
+export const FilterGroup: FC<FilterGroupProps> = ({ name, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  return (
+    <div className="rounded-lg bg-neutral-100">
+      <button
+        className="flex w-full items-center justify-between px-2.5 py-2.5 gap-4 text-sm font-semibold text-neutral-600"
+        onClick={toggleDropdown}
+      >
+        <span className="flex w-auto whitespace-nowrap">{name}</span>
+
+        <svg
+          stroke="currentColor"
+          fill="none"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className={` duration-200 ${isOpen ? "transition rotate-180" : "transition-transform rotate-0"}`}
+          height="1rem"
+          width="1rem"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <polyline points="6 9 12 15 18 9"></polyline>
+        </svg>
+      </button>
+
+      <div
+        className={`transition-all duration-200 ease-in-out ${isOpen ? "h-auto" : "h-0 overflow-hidden"}`}
+        aria-hidden={!isOpen}
+      >
+        <div
+          className={`${isOpen ? "transition-opacity duration-200 ease-in" : "opacity-0 hidden transition-opacity duration-200 ease-in"}`}
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
