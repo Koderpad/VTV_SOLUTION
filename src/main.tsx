b77cfe498f892556958663b28357b8b9f7ff878c
Koderpad <shen882002@gmai.com>
2024-05-16 17:28:15 +0700

    [Fix] error build

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ReduxProvider } from "./redux/provider.tsx";
-import { BrowserRouter } from "react-router-dom";
-
-const basename = '/vtv-solution.vercel.app';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
       <ReduxProvider>
         <App />
       </ReduxProvider>
-    </BrowserRouter>
   </React.StrictMode>
 );
