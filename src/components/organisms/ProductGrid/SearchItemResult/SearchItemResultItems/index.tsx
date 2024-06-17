431f917e51e7b37f08e16f2bbacddc3c2b3dc1d4
Koderpad <shen882002@gmai.com>
2024-06-18 02:18:06 +0700

    fix pagination results

--- src/components/organisms/ProductGrid/SearchItemResult/SearchItemResultItems/index.tsx
+++ src/components/organisms/ProductGrid/SearchItemResult/SearchItemResultItems/index.tsx
@@ -1,23 +1,113 @@
+import React, { useState } from "react";
 import { ProductDTO } from "@/utils/DTOs/common/Product/Response/ListProductPageResponse";
 import { CardItem } from "./CardItem";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 export const SearchItemResultItems = ({
   products,
 }: {
   products: ProductDTO[];
 }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4; // Số lượng sản phẩm trên mỗi trang
+
+  // Tính toán các sản phẩm cần hiển thị
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = products.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Tạo các nút điều hướng
+  const totalPages = Math.ceil(products.length / itemsPerPage);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handlePageClick = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
-    <div className="grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-      {products.length > 0 ? (
-        products.map((product) => (
-          <CardItem key={product.productId} product={product} />
-        ))
-      ) : (
-        <div>Không có sản phẩm nào</div>
-      )}
+    <div>
+      <div className="grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+        {currentItems.length > 0 ? (
+          currentItems.map((product) => (
+            <CardItem key={product.productId} product={product} />
+          ))
+        ) : (
+          <div>Không có sản phẩm nào</div>
+        )}
+      </div>
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious
+              onClick={handlePreviousPage}
+              className={currentPage === 1 ? "text-gray-500" : ""}
+            />
+          </PaginationItem>
+          {Array.from({ length: totalPages }, (_, index) => (
+            <PaginationItem key={index}>
+              <PaginationLink
+                isActive={currentPage === index + 1}
+                onClick={() => handlePageClick(index + 1)}
+              >
+                {index + 1}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+          <PaginationItem>
+            <PaginationNext
+              onClick={handleNextPage}
+              className={currentPage === totalPages ? "text-gray-500" : ""}
+            />
+          </PaginationItem>
+
+          {/* <PaginationNext */}
+          {/*   onClick={handleNextPage} */}
+          {/*   disabled={currentPage === totalPages} */}
+          {/* /> */}
+        </PaginationContent>
+      </Pagination>
     </div>
   );
 };
+// import { ProductDTO } from "@/utils/DTOs/common/Product/Response/ListProductPageResponse";
+// import { CardItem } from "./CardItem";
+//
+// export const SearchItemResultItems = ({
+//   products,
+// }: {
+//   products: ProductDTO[];
+// }) => {
+//   return (
+//     <div className="grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+//       {products.length > 0 ? (
+//         products.map((product) => (
+//           <CardItem key={product.productId} product={product} />
+//         ))
+//       ) : (
+//         <div>Không có sản phẩm nào</div>
+//       )}
+//     </div>
+//   );
+// };
 
 // export const SearchItemResultItems = ({
 //   products,
