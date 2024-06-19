a072e998198d86d3d314815a370bac03c1de01bb
Koderpad <shen882002@gmai.com>
2024-06-19 21:00:57 +0700

    update tooltip

--- src/components/organisms/Header/Tooltips/Account/index.tsx
+++ src/components/organisms/Header/Tooltips/Account/index.tsx
@@ -32,23 +32,27 @@ export const AccountTooltip: FC<AccountTooltipProps> = ({ username }) => {
         <h1 className=" flex justify-between items-center md:pr-0 pr-5 group">
           {username}
         </h1>
-        <div className="z-50 fixed -translate-x-40 hidden group-hover:block max-w-[100rem]">
-          {/* <div className="w-4 h-4 absolute right-8 bg-white rotate-45 transform -translate-y-1/2"></div> */}
-          <div className="bg-white shadow-lg rounded-lg ">
-            <div className="p-5 flex flex-col gap-4">
-              {content.map((item, index) => (
-                <div
-                  key={index}
-                  className="text-sm text-gray-600 whitespace-nowrap overflow-hidden text-ellipsis"
-                >
-                  <A
-                    href={item.link}
-                    className="flex items-center gap-2 hover:text-green-600"
+        <div className="z-40 relative">
+          <div className="absolute z-100 -top-2 -left-24 hidden group-hover:block w-auto max-w-md">
+            <div className="py-3">
+              <div className="w-4 h-4 left-24 absolute mt-1 bg-white rotate-45"></div>
+            </div>
+            <div className="py-3 bg-white shadow-lg rounded-lg ">
+              <div className="px-4 flex flex-col gap-2">
+                {content.map((item, index) => (
+                  <div
+                    key={index}
+                    className=" text-gray-600 whitespace-nowrap overflow-hidden text-ellipsis"
                   >
-                    {item.name}
-                  </A>
-                </div>
-              ))}
+                    <A
+                      href={item.link}
+                      className="flex items-center hover:text-blue-600"
+                    >
+                      {item.name}
+                    </A>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
