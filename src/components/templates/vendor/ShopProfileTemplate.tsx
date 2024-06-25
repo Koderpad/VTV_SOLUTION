cd881394f61d96d7a677a2902028a1b5e3a1d453
Koderpad <shen882002@gmai.com>
2024-06-25 20:27:00 +0700

    vendor 3

---
+++ src/components/templates/vendor/ShopProfileTemplate.tsx
@@ -0,0 +1,9 @@
+import { TemplateVendorV1 } from "./TemplateVendorV1";
+
+export const ShopProfileTemplate = () => {
+  return (
+    <TemplateVendorV1>
+      <h1>Shop Profile</h1>
+    </TemplateVendorV1>
+  );
+};
