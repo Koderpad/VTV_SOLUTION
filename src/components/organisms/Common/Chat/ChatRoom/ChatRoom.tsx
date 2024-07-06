70561b15c88c941c53ad175fcbb6bbe6a053e4d1
Koderpad <shen882002@gmail.com>
2024-07-06 20:56:52 +0700

    cus 9: fix chat part 2

--- src/components/organisms/Common/Chat/ChatRoom/ChatRoom.tsx
+++ src/components/organisms/Common/Chat/ChatRoom/ChatRoom.tsx
@@ -4,7 +4,6 @@ import { RootState } from "@/redux/store";
 import {
   selectMessages,
   selectFailedMessages,
-  addMessage,
   addFailedMessage,
   removeFailedMessage,
 } from "@/redux/features/common/chat/chatSlice";
@@ -42,15 +41,6 @@ const ChatRoom: React.FC<ChatRoomProps> = ({
 
   const updateMessages = useCallback(() => {
     if (data?.messageDTOs) {
-      data.messageDTOs.forEach((messageDTO) => {
-        const existingMessageIndex = messages.findIndex(
-          (message) => message.messengerId === messageDTO.messengerId
-        );
-        if (existingMessageIndex === -1) {
-          dispatch(addMessage(messageDTO));
-        }
-      });
-
       failedMessages.forEach((failedMessage) => {
         const existingMessageIndex = data.messageDTOs.findIndex(
           (messageDTO) => messageDTO.messengerId === failedMessage.messengerId
@@ -70,7 +60,7 @@ const ChatRoom: React.FC<ChatRoomProps> = ({
     if (!token || !username) return;
 
     const message: MessageDTO = {
-      messengerId: Date.now().toString(), // Simple unique ID generation
+      messengerId: Date.now().toString(),
       content,
       senderUsername: username,
       date: new Date().toISOString(),
@@ -85,10 +75,6 @@ const ChatRoom: React.FC<ChatRoomProps> = ({
         receiverUsername: receiverUsername,
       };
       await sendMessage(message_);
-      // if (!messages.some((m) => m.messengerId === message.messengerId)) {
-      //   dispatch(addMessage(message));
-      // }
-      // dispatch(addMessage(message));
     } catch (error) {
       console.error("Failed to send message:", error);
       dispatch(addFailedMessage(message));
@@ -127,235 +113,3 @@ const ChatRoom: React.FC<ChatRoomProps> = ({
 };
 
 export default ChatRoom;
-
-// import React, { useEffect, useState, useCallback } from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { RootState } from "@/redux/store";
-// import {
-//   selectMessages,
-//   selectFailedMessages,
-//   addMessage,
-//   addFailedMessage,
-//   removeFailedMessage,
-// } from "@/redux/features/common/chat/chatSlice";
-// import { useGetMessagesByRoomQuery } from "@/redux/features/common/chat/chatApiSlice";
-// import { sendMessage } from "@/utils/stock";
-// import ChatMessages from "../ChatMessages";
-// import ChatInput from "../ChatInput";
-// import { MessageDTO } from "@/utils/DTOs/chat/Response/ListMessagesPageResponse";
-
-// interface ChatRoomProps {
-//   roomChatId: string;
-// }
-
-// const ChatRoom: React.FC<ChatRoomProps> = ({ roomChatId }) => {
-//   const dispatch = useDispatch();
-//   const token = useSelector((state: RootState) => state.auth.token);
-//   const username = useSelector((state: RootState) => state.auth.user?.username);
-//   const messages = useSelector((state: RootState) =>
-//     selectMessages(state, roomChatId)
-//   );
-//   const failedMessages = useSelector((state: RootState) =>
-//     selectFailedMessages(state, roomChatId)
-//   );
-//   const [page, setPage] = useState(1);
-//   const { data, isLoading } = useGetMessagesByRoomQuery({
-//     roomChatId,
-//     page,
-//     size: 10,
-//   });
-
-//   const updateMessages = useCallback(() => {
-//     if (data?.messageDTOs) {
-//       data.messageDTOs.forEach((messageDTO) => {
-//         const existingMessageIndex = messages.findIndex(
-//           (message) => message.messengerId === messageDTO.messengerId
-//         );
-//         if (existingMessageIndex === -1) {
-//           dispatch(addMessage(messageDTO));
-//         }
-//       });
-
-//       failedMessages.forEach((failedMessage) => {
-//         const existingMessageIndex = data.messageDTOs.findIndex(
-//           (messageDTO) => messageDTO.messengerId === failedMessage.messengerId
-//         );
-//         if (existingMessageIndex === -1) {
-//           dispatch(addFailedMessage(failedMessage));
-//         }
-//       });
-//     }
-//   }, [data, dispatch, messages, failedMessages]);
-
-//   useEffect(() => {
-//     updateMessages();
-//   }, [updateMessages]);
-
-//   const handleSendMessage = async (content: string) => {
-//     if (!token || !username) return;
-
-//     const message: MessageDTO = {
-//       messengerId: Date.now().toString(), // Simple unique ID generation
-//       content,
-//       senderUsername: username,
-//       date: new Date().toISOString(),
-//       usernameSenderDelete: false,
-//       usernameReceiverDelete: false,
-//       roomChatId,
-//     };
-
-//     dispatch(addMessage(message));
-
-//     try {
-//       await sendMessage(message);
-//     } catch (error) {
-//       console.error("Failed to send message:", error);
-//       dispatch(removeFailedMessage(message));
-//       dispatch(addFailedMessage(message));
-//     }
-//   };
-
-//   const handleLoadMore = () => {
-//     setPage((prevPage) => prevPage + 1);
-//   };
-
-//   return (
-//     <div className="flex flex-col h-full">
-//       <div className="flex-grow overflow-y-auto">
-//         {isLoading ? (
-//           <div>Loading...</div>
-//         ) : (
-//           <>
-//             <ChatMessages
-//               messages={messages}
-//               failedMessages={failedMessages}
-//               currentUsername={username!}
-//             />
-//             {data && data.totalPage > page && (
-//               <button className="text-blue-500" onClick={handleLoadMore}>
-//                 Load more
-//               </button>
-//             )}
-//           </>
-//         )}
-//       </div>
-//       <div className="mt-4">
-//         <ChatInput onSendMessage={handleSendMessage} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ChatRoom;
-
-// import React, { useEffect, useState } from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { RootState } from "@/redux/store";
-// import {
-//   selectMessages,
-//   selectFailedMessages,
-//   addMessage,
-//   addFailedMessage,
-//   removeFailedMessage,
-// } from "@/redux/features/common/chat/chatSlice";
-// import { useGetMessagesByRoomQuery } from "@/redux/features/common/chat/chatApiSlice";
-// import { sendMessage } from "@/utils/stock";
-// import ChatMessages from "../ChatMessages";
-// import ChatInput from "../ChatInput";
-// import { MessageDTO } from "@/utils/DTOs/chat/Response/ListMessagesPageResponse";
-
-// interface ChatRoomProps {
-//   roomChatId: string;
-// }
-
-// const ChatRoom: React.FC<ChatRoomProps> = ({ roomChatId }) => {
-//   const dispatch = useDispatch();
-//   const token = useSelector((state: RootState) => state.auth.token);
-//   const username = useSelector((state: RootState) => state.auth.user?.username);
-//   const messages = useSelector((state: RootState) =>
-//     selectMessages(state, roomChatId)
-//   );
-
-//   const failedMessages = useSelector((state: RootState) =>
-//     selectFailedMessages(state, roomChatId)
-//   );
-//   const [page, setPage] = useState(1);
-//   const { data, isLoading } = useGetMessagesByRoomQuery({
-//     roomChatId,
-//     page,
-//     size: 10,
-//   });
-
-//   useEffect(() => {
-//     if (data && data.messageDTOs) {
-//       console.log("data khi loading message", data);
-//       // Thêm các tin nhắn mới vào Redux store
-//       data.messageDTOs.forEach((messageDTO) => {
-//         const existingMessageIndex = messages.findIndex(
-//           (message) => message.messengerId === messageDTO.messengerId
-//         );
-//         if (existingMessageIndex === -1) {
-//           dispatch(addMessage(messageDTO));
-//         }
-//       });
-
-//       // Thêm các tin nhắn gửi thất bại vào Redux store
-//       failedMessages.forEach((failedMessage) => {
-//         const existingMessageIndex = data.messageDTOs.findIndex(
-//           (messageDTO) => messageDTO.messengerId === failedMessage.messengerId
-//         );
-//         if (existingMessageIndex === -1) {
-//           dispatch(addFailedMessage(failedMessage));
-//         }
-//       });
-//     }
-//   }, [data, dispatch, roomChatId, messages, failedMessages]);
-
-//   const handleSendMessage = (content: string) => {
-//     if (token) {
-//       const message: MessageDTO = {
-//         messengerId: "", // Hàm tạo messengerId duy nhất
-//         content,
-//         senderUsername: username || "", // Thay đổi senderUsername tương ứng
-//         date: new Date().toISOString(), // Thời gian hiện tại
-//         usernameSenderDelete: false,
-//         usernameReceiverDelete: false,
-//         roomChatId,
-//       };
-//       dispatch(addMessage(message));
-//       sendMessage(message);
-//     }
-//   };
-
-//   const handleLoadMore = () => {
-//     setPage((prevPage) => prevPage + 1);
-//   };
-
-//   return (
-//     <div className="flex flex-col h-full">
-//       <div className="flex-grow overflow-y-auto">
-//         {isLoading ? (
-//           <div>Loading...</div>
-//         ) : (
-//           <>
-//             <ChatMessages
-//               messages={messages}
-//               failedMessages={failedMessages}
-//               currentUsername={username!}
-//             />
-//             {data && data.totalPage > page && (
-//               <button className="text-blue-500" onClick={handleLoadMore}>
-//                 Load more
-//               </button>
-//             )}
-//           </>
-//         )}
-//       </div>
-//       <div className="mt-4">
-//         <ChatInput onSendMessage={handleSendMessage} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ChatRoom;
--- src/pages/common/ChatPage.tsx
+++ src/pages/common/ChatPage.tsx
@@ -41,7 +41,7 @@ const ChatPage: React.FC = () => {
 
   useEffect(() => {
     const setupRoomChats = async () => {
-      if (data?.roomChatDTOs && isSocketConnected) {
+      if (data?.roomChatDTOs && isSocketReady) {
         console.log("Setting room chats...");
         dispatch(setRoomChats(data.roomChatDTOs));
 
@@ -59,7 +59,7 @@ const ChatPage: React.FC = () => {
     };
 
     setupRoomChats();
-  }, [data, isSocketConnected, dispatch]);
+  }, [data, isSocketReady, dispatch]);
 
   const handleChatIconClick = () => {
     setIsChatOpen(!isChatOpen);
@@ -74,94 +74,3 @@ const ChatPage: React.FC = () => {
 };
 
 export default ChatPage;
-
-// import React, { useState, useEffect } from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { RootState } from "@/redux/store";
-// import Chat from "@/components/organisms/Common/Chat/Chat";
-// import { useGetRoomChatListQuery } from "@/redux/features/common/chat/chatApiSlice";
-// import { setRoomChats } from "@/redux/features/common/chat/chatSlice";
-// import {
-//   initSocket,
-//   disconnectSocket,
-//   isSocketConnected,
-//   subscribeToMultipleRooms,
-// } from "@/utils/stock/configSocket";
-// import ChatIcon from "@/components/organisms/Common/Chat/ChatIcon/ChatIcon";
-
-// const ChatPage: React.FC = () => {
-//   const [isChatOpen, setIsChatOpen] = useState(false);
-//   const dispatch = useDispatch();
-//   const token = useSelector((state: RootState) => state.auth.token);
-//   const { data, isLoading } = useGetRoomChatListQuery({ page: 1, size: 10 });
-
-//   useEffect(() => {
-//     const fetchSocket = async () => {
-//       if (token) {
-//         await initSocket(token)
-//           .then(() => console.log("Socket connected successfully"))
-//           .catch((error) => console.error("Socket connection failed:", error));
-//       }
-//     };
-
-//     fetchSocket();
-
-//     return () => {
-//       disconnectSocket();
-//     };
-//   }, [token]);
-
-//   useEffect(() => {
-//     const fetchSub = async () => {
-//       if (data && data?.roomChatDTOs && isSocketConnected) {
-//         console.log("Setting room chats...");
-//         await dispatch(setRoomChats(data.roomChatDTOs));
-
-//         if (isSocketConnected()) {
-//           console.log("Start Subscribing to rooms...");
-//           const roomChatIds = data.roomChatDTOs.map((room) => room.roomChatId);
-//           await subscribeToMultipleRooms(roomChatIds)
-//             .then(() => console.log("Subscribed to all rooms"))
-//             .catch((error) =>
-//               console.error("Failed to subscribe to rooms:", error)
-//             );
-//         }
-//       }
-//     };
-//     fetchSub();
-//   }, [data, isSocketConnected, dispatch]);
-
-//   const handleChatIconClick = () => {
-//     setIsChatOpen(!isChatOpen);
-//   };
-
-//   return (
-//     <>
-//       <ChatIcon onClick={handleChatIconClick} />
-//       {isChatOpen && <Chat />}
-//     </>
-//   );
-// };
-
-// export default ChatPage;
-
-// import React, { useState } from "react";
-// import Chat from "@/components/organisms/Common/Chat/Chat";
-// import ChatIcon from "@/components/organisms/Common/Chat/ChatIcon/ChatIcon";
-
-// const ChatPage: React.FC = () => {
-//   const [isChatOpen, setIsChatOpen] = useState(false);
-
-//   const handleChatIconClick = () => {
-//     setIsChatOpen(!isChatOpen);
-//   };
-
-//   return (
-//     <>
-//       <ChatIcon onClick={handleChatIconClick} />
-//       {isChatOpen && <Chat />}
-//     </>
-//   );
-// };
-
-// export default ChatPage;
